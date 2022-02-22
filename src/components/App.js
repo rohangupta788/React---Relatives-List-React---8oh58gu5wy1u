98a44876487e9f486dab55c492e4e345773bc3e0
rohangupta788 <rohangupta788@gmail.com>
2022-02-22 11:27:04 +0530

    First

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import List from "./List";
+
+
+const family = ["Rohan","Rahul","Shailesh","Akash"]
 
 class App extends Component {
+
     render() {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               <ol>
+                   {
+                    family.map((member,index)=>{
+                    const key = `relativeListItem${index +1}`
+                       return <List name={member} key = {key} />
+                       })
+                   }
+                   </ol>
             </div>
         )
     }
